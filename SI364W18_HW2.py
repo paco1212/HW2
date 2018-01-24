6a6dd22bf4c6cf84271fe3faaee08213e7809c18
paco1212 <frga@umich.edu>
2018-01-24 15:58:21 -0500

    Added Routes and Forms

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -11,10 +11,12 @@
 #############################
 ##### IMPORT STATEMENTS #####
 #############################
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, RadioField, ValidationError
 from wtforms.validators import Required
+import requests
+import json
 
 #####################
 ##### APP SETUP #####
@@ -27,6 +29,10 @@ app.config['SECRET_KEY'] = 'hardtoguessstring'
 ###### FORMS #######
 ####################
 
+class AlbumEntryForm(FlaskForm):
+	album_name = StringField('Enter the name of an album:', validators =[Required()])
+	ranking = RadioField('How much do you like this album?', choices = [('1','1'), ('2', '2'), ('3','3')])
+	submit = SubmitField()
 
 
 
@@ -42,6 +48,66 @@ def hello_world():
 @app.route('/user/<name>')
 def hello_user(name):
     return '<h1>Hello {0}<h1>'.format(name)
+
+
+####################
+###### PART 1 ######
+####################
+
+@app.route('/artistform')
+def queryForm():
+	return render_template('artistform.html')
+
+
+@app.route('/artistinfo', methods = ['GET', 'POST'])
+def queryResult():
+	if request.method == 'GET':
+		# Get the artist name from the user's form
+		query = request.args.get('artist')
+		# Prepare to make a request to the iTunes API
+		baseurl = 'https://itunes.apple.com/search'
+		params = {'term':query}
+		# Make a request to the iTunes API
+		resp = requests.get(baseurl, params = params)
+		text = json.loads(resp.text)
+		results = text['results']
+		return render_template('artist_info.html', objects = results)
+	return redirect(url_for('queryForm'))
+
+@app.route('/artistlinks')
+def links():
+	return render_template('artist_links.html')
+
+@app.route('/specific/song/<artist_name>')
+def songDisplay(artist_name):
+	# Prepare to make a request to the iTunes API
+	baseurl = 'https://itunes.apple.com/search'
+	params = {'term':artist_name}
+	# Make a request to the iTunes API
+	resp = requests.get(baseurl, params = params)
+	text = json.loads(resp.text)
+	results = text['results']
+	return render_template('specific_artist.html', results = results)
+
+
+####################
+###### PART 2 ######
+####################
+
+@app.route('/album_entry')
+def albumForm():
+	album_form = AlbumEntryForm()
+	return render_template('album_entry.html', form = album_form)
+
+@app.route('/album_data', methods = ['GET', 'POST'])
+def albumData():
+	data_form = AlbumEntryForm(request.form)
+	if request.method =='POST':
+		album_name = data_form.album_name.data
+		rank = data_form.ranking.data
+	
+
+	return render_template('album_data.html', album = album_name, stars = rank)
 
 
 if __name__ == '__main__':
